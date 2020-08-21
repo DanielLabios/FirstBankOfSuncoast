8f1b243390835eaf9590c7ec632ba3025a1a3ba1
Daniel Labios <labiosd@gmail.com>
2020-08-21 11:03:48 -0400

    Class Transaction, AccountBalance() and GetTransactionAmount() implemented

--- Program.cs
+++ Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Transaction
     {
-      Console.WriteLine("Welcome to C#");
+        public double amount { get; set; }
+        public string checkingOrSavings { get; set; }
+        public string withdrawalOrDeposit { get; set; }
     }
-  }
+    class Program
+    {
+        public double AccountBalance(string cOrS, List<Transaction> history)
+        {
+            var accountType = new List<Transaction>();
+            double total = 0.00;
+            accountType.AddRange(history.Where(history => history.checkingOrSavings == cOrS));
+            foreach (Transaction transaction in accountType)
+            {
+                if (transaction.withdrawalOrDeposit == "W")
+                {
+                    total = total - transaction.amount;
+                }
+                else if (transaction.withdrawalOrDeposit == "D")
+                {
+                    total = total + transaction.amount;
+                }
+            }
+            return total;
+        }
+        public double GetTransactionAmount()
+        {
+            double amount;
+            bool trueDouble = false;
+            while (trueDouble == false)
+            {
+                Console.WriteLine("Please enter the amount:"); //Later, implement try counter == 5 and quit
+                trueDouble = double.TryParse(Console.ReadLine(), out amount);
+                if (trueDouble == false)
+                {
+                    Console.WriteLine("Sorry, we couldn't recognize the amount type. Please try again. \r\n");
+                }
+                else if (amount < 0)
+                {
+                    Console.WriteLine("Sorry, the specified amount was negative. Please input the amount in positive form");
+                    trueDouble = false;
+                }
+            }
+            if (amount == 0)
+            {
+                Console.WriteLine("The specified amount was $0.00, which means no change will happen to your balance.\r\n Returning to the main menu.\r\n");
+                amount = -1;
+            }
+            return amount;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+        }
+    }
 }
