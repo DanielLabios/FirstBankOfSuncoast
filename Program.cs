f4e7940665ee76634e1a75ceb723edfe4dded872
Daniel Labios <labiosd@gmail.com>
2020-08-21 12:10:29 -0400

    class and methods created. Starting on algorithm

--- Program.cs
+++ Program.cs
@@ -34,18 +34,30 @@ namespace FirstBankOfSuncoast
         {
             double amount;
             bool trueDouble = false;
+            int tries = -1;
             while (trueDouble == false)
             {
-                Console.WriteLine("Please enter the amount:"); //Later, implement try counter == 5 and quit
-                trueDouble = double.TryParse(Console.ReadLine(), out amount);
+                tries++;
+                if (tries == 5)
+                {
+                    Console.WriteLine("You have exceeded the amount of tries alloted for this attempt.");
+                    trueDouble = true;
+                    amount = -1;
+                }
                 if (trueDouble == false)
                 {
-                    Console.WriteLine("Sorry, we couldn't recognize the amount type. Please try again. \r\n");
+                    Console.WriteLine("Please enter the amount:"); // Later implement quit option
+                    trueDouble = double.TryParse(Console.ReadLine(), out amount);
+                }
+                if (trueDouble == false)
+                {
+                    Console.WriteLine("Sorry, we couldn't recognize the amount type.");
                 }
                 else if (amount < 0)
                 {
                     Console.WriteLine("Sorry, the specified amount was negative. Please input the amount in positive form");
                     trueDouble = false;
+                    tries--;
                 }
             }
             if (amount == 0)
@@ -54,6 +66,39 @@ namespace FirstBankOfSuncoast
                 amount = -1;
             }
             return amount;
+        }
+        public bool CheckBounce(string cOrS, List<Transaction> balance, double amount)
+        {
+            var total = AccountBalance(cOrS, balance);
+            if (total >= amount)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public int CheckUserChoice(int options, int tryinput)
+        {
+            int choice;
+            bool trueInt = false;
+            int tries = -1;
+            while (trueInt == false)
+            {
+                tries++;
+                if (tries == tryinput && tries != 1)
+                {
+                    Console.WriteLine("You have exceeded the amount of tries alloted for this attempt.");
+                    trueInt = true;
+                    choice = 1;
+                }
+                trueInt = int.TryParse(Console.ReadLine(), out choice);
+                if (trueInt == false || choice > options || choice <= 0)
+                {
+                    Console.WriteLine("Input was not recognized. Try again.");
+                    trueInt = false;
+                }
+            }
+            return choice;
         }
         static void Main(string[] args)
         {
